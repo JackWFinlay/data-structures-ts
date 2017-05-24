fb2ffa598e5b92732f8e3b1c0f21838adeb748e5
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-05-24 16:29:48 +1200

    Re-order methods

--- src/ts/IEnumerable.ts
+++ src/ts/IEnumerable.ts
@@ -1,9 +1,10 @@
 import { ListNode } from './ListNode';
 export interface IEnumerable {
+    add<T>(index: number,node: ListNode<T>);
     enumerate<T>(): ListNode<T>[];
     get<T>(index: number): ListNode<T>;
-    add<T>(index: number,node: ListNode<T>);
+    peek<T>(): ListNode<T>;
     push<T>(node: ListNode<T>): number;
     pop<T>(): ListNode<T>;
-    peek<T>(): ListNode<T>;
+
 }
